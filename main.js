02e4d299203accb1d23c5dce0900e5e5c4f70afc
Johno Caldwell <jjcldwll@gmail.com>
2022-04-01 16:40:55 -0400

    Implement Reset button functionality

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16*16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+const generateGrid = x => {
+    container.replaceChildren();
+    container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    for (let i = 0; i < x ** 2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
 }
 
+generateGrid(16);
+
 const button = document.getElementById("reset");
+button.addEventListener("click", function (e) {
+    const result = Number(window.prompt("What size?"));
+    if (isNaN(result) || result > 100) {
+        window.alert("Invalid value.");
+    }
+    generateGrid(result);
+});
